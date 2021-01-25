905b714affa8341d9e543361f89d54340baffe34
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-01-25 13:06:30 +0100

    ...

--- v2/dom-manipulate-with-jquery.js
+++ v2/dom-manipulate-with-jquery.js
@@ -1,17 +1,6 @@
-// We use the queryselector + innerHTML
-// to replace content in exisiting elements in the DOM/HTML
-document.querySelector('h3').innerHTML = 'Hej från JavaScript!';
-
-// We can also create brand new elements
-let aPtag = document.createElement('p');
-// Add text/content inside them
-aPtag.innerHTML = 'I am a paragraph created from JavaScript'
-// And append/add them last in the DOM
-document.querySelector('body').append(aPtag);
-
-// We can also prepend new elements
-let anImageTag = document.createElement('img');
-// Add a src 
-anImageTag.src = 'https://java20gbg.lms.nodehill.se/uploads/images/2021-01-3T17-57-16/billie-i-hangmattan.jpg';
-// Prepend add something a the top/first
-document.querySelector('body').prepend(anImageTag);
+// Grab the h3 tag and replace its content
+$('h3').html('Hej på dig!');
+// Append a p-tag to the end of the body
+$('body').append('<p>I am a paragraph created from JavaScript!</p>');
+// Append an image tag to the start of the body
+$('body').prepend('<img src="https://java20gbg.lms.nodehill.se/uploads/images/2021-01-3T17-57-16/billie-i-hangmattan.jpg">');
