c0aab0d5fd866ea17a6679c9041fb2865fed45b5
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-01-28 17:01:51 +0100

    how to grab elements

--- v2/theDOM/sandbox.js
+++ v2/theDOM/sandbox.js
@@ -1,3 +1,19 @@
-const para = document.querySelector('p');
+// Grab an html element
+// const para = document.querySelector('p');
+// const para2 = document.querySelector('.error');
 
-console.log(para);
+// console.log(para2);
+
+// Grab several elements (not an array, NODELIST)
+// const paras = document.querySelectorAll('p');
+
+// paras.forEach(para => {
+//   console.log(para);
+// }
+// );
+
+// const errors = document.querySelectorAll('.error');
+
+// console.log(errors);
+
+const title = document
