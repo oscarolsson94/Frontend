a58acef14adf6ad0a3445648048c69c25784b50b
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-01-28 21:40:51 +0100

    input type

--- v2/JQueryCrashCourse/jQueryTest.js
+++ v2/JQueryCrashCourse/jQueryTest.js
@@ -1,1 +1,19 @@
-$('h1').hide();
+$('h1').hide(); // Hides element
+
+$('h1#heading1').hide(); // Using id to grab
+
+$('h1.heading2').hide(); // Using class to grab
+
+$('p span').css('color', 'red'); // Using CSS property to change color
+
+$('ul#list li: first').css('color', 'red'); // Changing color of first child element
+$('ul#list li: last').css('color', 'green'); // Changing color of last child element
+$('ul#list li: even').css('background-color', 'yellow'); // Changing background-color color on even items
+$('ul#list li: odd').css('background-color', '#cccccc'); // Changing background-color color on odd items
+
+$('ul#list li: nth-child(3n)').css('list-style', 'none'); // Removing bullet point from every 3rd element in list
+
+$(':button').hide(); // Using input types to grab
+$(':submit').hide();
+$(':text').hide();
+
