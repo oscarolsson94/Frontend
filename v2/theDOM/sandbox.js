e73d5e31290e57011705544ca73502a77a232340
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-01-28 17:30:20 +0100

    more js

--- v2/theDOM/sandbox.js
+++ v2/theDOM/sandbox.js
@@ -1,10 +1,10 @@
-// Grab an html element
+// Grab an html element ---
 // const para = document.querySelector('p');
 // const para2 = document.querySelector('.error');
 
 // console.log(para2);
 
-// Grab several elements (not an array, NODELIST)
+// Grab several elements (not an array, NODELIST, can use forEach) ---
 // const paras = document.querySelectorAll('p');
 
 // paras.forEach(para => {
@@ -16,4 +16,48 @@
 
 // console.log(errors);
 
-const title = document
+
+
+// get an element by ID ---
+// const title = document.getElementById('page-title');
+// console.log(title);
+
+// get elements by their class name ---
+// returns HTMLCollection, cant use forEach
+// const errors = document.getElementsByClassName('error');
+// console.log(errors);
+
+// get element by tag name ---
+// returns HTMLCollection
+// const paras = document.getElementsByTagName('p');
+// console.log(paras);
+// console.log(paras[1]);
+
+
+// Change content of element ---
+// const para = document.querySelector('p');
+// console.log(para.innerText);
+// para.innerText = 'ninjas are awesome';
+
+// Add text to all p elements ---
+// const paras = document.querySelectorAll('p');
+
+// paras.forEach(para => {
+//   para.innerText += ' new text';
+// });
+
+// Change html of an element ---
+
+//  const content = document.querySelector('.content');
+
+// console.log(content.innerHTML);
+// content.innerHTML += '<h2>THIS IS A NEW H2</h2>';
+
+// Make new p tag for each member of data set
+// const content = document.querySelector('.content');
+
+// const people = ['mario', 'luigi', 'yoshi'];
+
+// people.forEach(person => {
+//   content.innerHTML += `<p>${person}</p>`;
+// });
