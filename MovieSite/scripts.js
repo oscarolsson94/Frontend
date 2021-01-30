b64dbcfe36c4aec0f923c4a3f2d2819a11d6b6ff
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-01-30 13:15:23 +0100

    wroking on building page from json file

--- MovieSite/scripts.js
+++ MovieSite/scripts.js
@@ -1,7 +1,16 @@
-fetch("./movies.json")
-.then(response => {
-   return response.json();
-})
-  .then(data => console.log(data));
-
-  
+$(document).ready(function() {
+           
+            
+  $.getJSON('movies.json', function(json) {
+    $('main').html('<p> Title: ' + json.title + '</p>');
+    $('main').append('<p> Production year : ' + json.productionYear + '</p>');
+    $('main').append('<p> Length: ' + json.length + '</p>');
+    $('main').append('<p> Genre: ' + json.length + '</p>');
+    $('main').append('<p> Distributor: ' + json.length + '</p>');
+    $('main').append('<p> Language: ' + json.length + '</p>');
+    $('main').append('<p> Length: ' + json.length + '</p>');
+    $('main').append('<p> Length: ' + json.length + '</p>');
+    $('main').append('<p> Length: ' + json.length + '</p>');
+    $('main').append('<p> Length: ' + json.length + '</p>');
+    });
+  });
