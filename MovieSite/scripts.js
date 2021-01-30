4368a5bf64c9c4cdf7f8fc7490216472c519451a
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-01-30 13:27:29 +0100

    working on script

--- MovieSite/scripts.js
+++ MovieSite/scripts.js
@@ -2,15 +2,16 @@ $(document).ready(function() {
            
             
   $.getJSON('movies.json', function(json) {
-    $('main').html('<p> Title: ' + json.title + '</p>');
+    $('main').html('<div class= "movieimage" ');
+    $('main').append('<p> Title: ' + json.title + '</p>');
     $('main').append('<p> Production year : ' + json.productionYear + '</p>');
     $('main').append('<p> Length: ' + json.length + '</p>');
-    $('main').append('<p> Genre: ' + json.length + '</p>');
-    $('main').append('<p> Distributor: ' + json.length + '</p>');
-    $('main').append('<p> Language: ' + json.length + '</p>');
-    $('main').append('<p> Length: ' + json.length + '</p>');
-    $('main').append('<p> Length: ' + json.length + '</p>');
-    $('main').append('<p> Length: ' + json.length + '</p>');
-    $('main').append('<p> Length: ' + json.length + '</p>');
+    $('main').append('<p> Genre: ' + json.genre + '</p>');
+    $('main').append('<p> Distributor: ' + json.distributor + '</p>');
+    $('main').append('<p> Language: ' + json.language + '</p>');
+    $('main').append('<p> Subtitles: ' + json.subtitles + '</p>');
+    $('main').append('<p> Director: ' + json.director + '</p>');
+    $('main').append('<p> Actors: ' + json.actors + '</p>');
+    $('main').append('<p> Description: ' + json.description + '</p>');
     });
   });
