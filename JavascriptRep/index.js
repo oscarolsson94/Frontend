0d51024f681b74120ac8b44d875f6ec4ef1eb54f
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-02-01 14:52:11 +0100

    functions

--- JavascriptRep/index.js
+++ JavascriptRep/index.js
@@ -174,3 +174,47 @@
 
 // console.log(count);
 
+// Functions --------------------------------------
+
+// function add(num1, num2) {
+//   return num1 + num2;
+// }
+
+// console.log(add(10, 6));
+// console.log(add(15, 7));
+
+
+// function getUserCredentials(firstName, lastName, email) {
+//     return `${firstName} ${lastName} | ${email}`
+// }
+
+// const userCredentials = getUserCredentials('Dylan', 'Israel', 'DylansEmail310@gmail.com');
+
+// function getPreTaxTotal(cartItems) {
+
+//   let total = 0;
+
+//   cartItems.forEach(element => {
+//     total += element.quantity * element.price;
+//   });
+
+//   return total;
+// }
+
+// const cartItems = [
+//     { quantity: 1, price: 5 },
+//     { quantity: 3, price: 4 },
+//     { quantity: 10, price: 1}
+// ];
+
+// const preTaxTotal = getPreTaxTotal(cartItems);
+
+// function getTaxedTotal(preTaxTotal) {
+//   return preTaxTotal *= 1.08;
+// }
+
+// const finalTaxedTotal = getTaxedTotal(preTaxTotal);
+
+// console.log(userCredentials)
+// console.log(preTaxTotal);
+// console.log(finalTaxedTotal);
