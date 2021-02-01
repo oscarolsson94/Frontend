468533f15a1e43e587077f18e8d9c10ef0d260a2
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-02-01 13:14:56 +0100

    arrays

--- JavascriptRep/index.js
+++ JavascriptRep/index.js
@@ -16,20 +16,54 @@
 
 //Strings-------------------------
 
-let name = "Dylan";
+// let name = "Dylan";
 
-console.log(typeof name);
+// console.log(typeof name);
 
-let firstName = "Oscar";
-let lastName = "Olsson";
+// let firstName = "Oscar";
+// let lastName = "Olsson";
 
-console.log(`${firstName} ${lastName}`.length); // template string
+// console.log(`${firstName} ${lastName}`.length); // template string
 
-console.log(`${firstName} ${lastName}             `.trim().length); // removes all spaces before and after content
+// console.log(`${firstName} ${lastName}             `.trim().length); // removes all spaces before and after content
 
-console.log(`${firstName} ${lastName}`.toUpperCase());
+// console.log(`${firstName} ${lastName}`.toUpperCase());
 
-console.log(`${firstName} ${lastName}`.toLowerCase());
+// console.log(`${firstName} ${lastName}`.toLowerCase());
 
-console.log(`${firstName} ${lastName}`.split(' ')); // returns array of the string, split at every space
+// console.log(`${firstName} ${lastName}`.split(' ')); // returns array of the string, split at every space
 
+// let example = 7.77;  // Only one type of number for floats,int, long etc
+
+// console.log(example);
+
+// console.log(parseInt(example));
+
+// console.log(parseFloat(example));
+
+// console.log(example.toFixed(5)); //print the variable with 5 decimal numbers
+
+// console.log(example.toFixed(1)); //print the variable with 1 decimal number (rounds it up)
+
+// // toFixed - method converts the number to a String
+
+// Arrays--------------------------
+
+// let exampleArray = [5, 6, 7];
+
+// console.log(exampleArray.length);
+
+// console.log(exampleArray[0]);
+
+// exampleArray.push(1, 2, 3);
+
+// console.log(exampleArray);
+
+// exampleArray.pop();
+// exampleArray.pop();
+
+// console.log(exampleArray);
+
+// exampleArray.forEach((element) => {
+//   console.log(element);
+// });
