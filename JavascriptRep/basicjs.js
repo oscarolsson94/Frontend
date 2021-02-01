2e63418d8a6b4b7759feb5b43f3cfe4fa359a615
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-02-01 17:40:17 +0100

    destructuring objects

--- JavascriptRep/basicjs.js
+++ JavascriptRep/basicjs.js
@@ -222,17 +222,51 @@
 
 // Template literals----------------------------------------
 
-let word1 = "Oscar";
-let word2 = "Olsson";
-let num1 = 2;
-let num2 = 3;
+// let word1 = "Oscar";
+// let word2 = "Olsson";
+// let num1 = 2;
+// let num2 = 3;
 
-const fullName = `${num1 + num2} ${word2}`;
+// const fullName = `${num1 + num2} ${word2}`;
 
-// let example = "Hello \n" + "world"; // Multi line strings
-let example = `${"Hello"} 
-${"World"}
-`;
+// // let example = "Hello \n" + "world"; // Multi line strings
+// let example = `${"Hello"} 
+// ${"World"}
+// `;
 
-document.getElementById("example").innerText = example;
-// console.log(example);
+// document.getElementById("example").innerText = example;
+// // console.log(example);
+
+// Destructuring objects-------------------------------
+
+// const player = {
+//   name: 'Lebron James',
+//   club: 'LA Lakers',
+//   address: {
+//     city: 'Los Angeles'
+//   }
+// };
+
+// // player.name;
+// // player.club;
+
+// const { name, club, address: { city } } = player; // Take the values out of the object
+
+// console.log(`${name} plays for ${club} and lives in ${city}`);
+
+// challenge:
+
+// const student = {
+//     name: "Kyle",
+//     age: 24,
+//     projects: {
+//         diceGame: "Two player dice game using JavaScript"
+//     }
+// }
+
+// const { name, age, projects: { diceGame } } = student;
+
+// console.log(`${name} ${age} ${diceGame}`);
+
+// Destructuring Arrays-------------------------------------
+
