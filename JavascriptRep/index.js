12e486b9e3d68cfae63e991517746f8cf14a9d01
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-02-01 12:56:00 +0100

    code along

--- JavascriptRep/index.js
+++ JavascriptRep/index.js
@@ -1,5 +1,35 @@
-let dogAge = 10;
-dogAge++;
-let dogKind = "retriever";
+// let dogAge = 10;
+// dogAge++;
+// let dogKind = "retriever";
 
-console.log("The dog is a " + dogKind + ", it is " + dogAge + " years old");
+// console.log("The dog is a " + dogKind + ", it is " + dogAge + " years old");
+
+// const buildDate = "October, 25, 2010"; // must be given a value at initialization
+
+// let releasedDate;
+// releasedDate = "October, 27th, 2011";
+
+// const name = "Gator";
+
+// let age = 10;
+// age++;
+
+//Strings-------------------------
+
+let name = "Dylan";
+
+console.log(typeof name);
+
+let firstName = "Oscar";
+let lastName = "Olsson";
+
+console.log(`${firstName} ${lastName}`.length); // template string
+
+console.log(`${firstName} ${lastName}             `.trim().length); // removes all spaces before and after content
+
+console.log(`${firstName} ${lastName}`.toUpperCase());
+
+console.log(`${firstName} ${lastName}`.toLowerCase());
+
+console.log(`${firstName} ${lastName}`.split(' ')); // returns array of the string, split at every space
+
