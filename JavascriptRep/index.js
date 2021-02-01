53ef9818e0a22e35488255aa97bfce6b132401aa
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-02-01 12:39:56 +0100

    first les

---
+++ JavascriptRep/index.js
@@ -0,0 +1,12 @@
+let firstName = "Dylan";
+
+firstName = "Zack";
+
+let lastName = "Israel";
+let fullName = firstName + " " + lastName;
+
+let age = 32;
+let isEmployed = true;
+isEmployed = false;
+
+console.log(fullName);
