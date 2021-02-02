fe77809126f89ad98c309e620da08be8f70e8d18
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-02-02 20:07:01 +0100

    includes()-method

--- JavascriptRep/es6.js
+++ JavascriptRep/es6.js
@@ -169,3 +169,18 @@
 // }
 
 // foodShopping();
+
+// includes() --------------------------------------------------------
+
+// let numArray = [1, 2, 3, 4, 5];
+
+// console.log(numArray.includes(2)) // returns false or true
+
+// const listIngredients =  [ "flour", "sugar", "eggs", "butter" ];
+
+// if( listIngredients.includes("chocolate")) {
+//     console.log("We are going to make a chocolate cake" );
+// } else {
+//     console.log("We can't make a chocolate cake because we are missing the ingredient chocolate" );
+// }
+
