e973882b5ad747153ac7f1e4aa57ffdd9f88ac37
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-02-03 12:40:52 +0100

    loading photos from json html

--- v3/code-along1/main.js
+++ v3/code-along1/main.js
@@ -1,3 +1,18 @@
 // https://jsonplaceholder.typicode.com/albums/
 // https://jsonplaceholder.typicode.com/albums/1/photos
 
+
+loadPhotos();
+
+// Ladda in bilder från JSON-fil till en array
+async function loadPhotos() {
+  let photos = await $.getJSON("https://jsonplaceholder.typicode.com/albums/1/photos");
+  renderPhotos(photos);
+}
+
+function renderPhotos(photos) {
+  photos.forEach(photo => {
+    
+  });
+}
+
