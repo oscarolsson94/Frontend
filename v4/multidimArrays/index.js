2db067c5dc4104d9621f8e666b6406d79c62a79e
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-02-08 13:46:34 +0100

    multi arrays

---
+++ v4/multidimArrays/index.js
@@ -0,0 +1,14 @@
+let a = [1, 2, 3];
+
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
+
+
+let a = [
+    [' ', ' ', ' '],
+    [' ', 'x', ' '],
+    [' ', ' ', ' ']
+]
+
+console.log(a[1][1]);
