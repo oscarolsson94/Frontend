4bf2d966a6af1d7e1145eedf9686b97e28f235e7
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-02-08 13:53:05 +0100

    flat on multi dim arrays

--- v4/multidimArrays/index.js
+++ v4/multidimArrays/index.js
@@ -4,6 +4,10 @@
 // console.log(a[1]);
 // console.log(a[2]);
 
+// In javaScript a multidimensional array
+// in this case two dimensional
+// is really just an array of arrays...
+// (there is no special syntax)
 
 // let a = [
 //     [' ', ' ', ' '],
@@ -12,4 +16,12 @@
 // ]
 
 // console.log(a[1][1]);
+// Middle
 
+// You can flatten a multidimensional array
+
+let flatA = a.flat(); // some calculations will be fast on 1-dimensional arrays
+console.log('Flat', flatA) // will make the array 1-dimensional
+// row*columns+columns
+console.log(flat[1 * 3 + 1]);
+// Middle
