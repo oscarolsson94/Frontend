674707e505a770351b064fe753a6f00fb53a53c6
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-02-09 16:26:21 +0100

    setting up routes

---
+++ restAPImongoDB/routes/subscribers.js
@@ -0,0 +1,34 @@
+import express from 'express';
+
+const router = express.Router();
+
+// Getting all
+router.get('/', (reg, res) => {
+    
+});
+// Getting one
+router.get('/:id', (reg, res) => {
+    
+});
+// Creating one
+router.post('/', (reg, res) => {
+    
+});
+// Updating one
+router.patch('/:id', (reg, res) => {
+    
+});
+// Deleting one
+router.delete('/:id', (reg, res) => {
+    
+});
+
+
+
+
+
+
+
+
+
+export default router;
