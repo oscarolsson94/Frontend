e67d0f2733cd10cfdb5dfa1198b580d61b0d184f
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-02-09 16:00:01 +0100

    connection setup to mongoDB

--- restAPImongoDB/server.js
+++ restAPImongoDB/server.js
@@ -1,6 +1,13 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
 const app = express();
 const PORT = 3000;
 
+mongoose.connect('mongodb+srv://test:test123@cluster0.gfsfw.mongodb.net/oscarDB?retryWrites=true&w=majority', { useNewUrlParser: true , useUnifiedTopology: true });
+const db = mongoose.connection;
+
 app.listen(PORT, () => console.log(`server started on port: ${PORT}`));
+
+db.on('error', (error) => console.error(error))
+db.once('open', () => console.log('connected to database'))
