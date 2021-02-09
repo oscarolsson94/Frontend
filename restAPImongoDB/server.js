ae9f1aad61b042ed3335a87b1c91c953da3a0adf
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-02-09 17:29:57 +0100

    working api

--- restAPImongoDB/server.js
+++ restAPImongoDB/server.js
@@ -1,13 +1,19 @@
-import express from 'express';
-import mongoose from 'mongoose';
+require('dotenv').config()
 
-const app = express();
-const PORT = 3000;
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
 
-mongoose.connect('mongodb+srv://test:test123@cluster0.gfsfw.mongodb.net/oscarDB?retryWrites=true&w=majority', { useNewUrlParser: true , useUnifiedTopology: true });
-const db = mongoose.connection;
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
+const db = mongoose.connection
+db.on('error', (error) => console.error(error))
+db.once('open', () => console.log('Connected to Database'))
 
-app.listen(PORT, () => console.log(`server started on port: ${PORT}`));
+app.use(express.json())
 
-db.on('error', (error) => console.error(error));
-db.once('open', () => console.log('connected to database'));
+const subscribersRouter = require('./routes/subscribers')
+app.use('/subscribers', subscribersRouter)
+
+app.listen(3000, () => console.log('Server Started'))
+
+
