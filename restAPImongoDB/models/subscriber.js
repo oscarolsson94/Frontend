d1713ce0d232a41958ca806dd9d96f6df8f6f9cc
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-02-09 17:34:37 +0100

    working api done

--- restAPImongoDB/models/subscriber.js
+++ restAPImongoDB/models/subscriber.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 const subscriberSchema = new mongoose.Schema({
   name: {
@@ -16,4 +16,4 @@ const subscriberSchema = new mongoose.Schema({
   }
 })
 
-module.exports = mongoose.model('Subscriber', subscriberSchema)
+module.exports = mongoose.model('Subscriber', subscriberSchema);
