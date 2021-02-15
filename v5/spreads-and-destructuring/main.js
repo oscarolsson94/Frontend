186c6e271663d0c0c00088368f14f57b35757277
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-02-15 08:45:53 +0100

    spread operators

---
+++ v5/spreads-and-destructuring/main.js
@@ -0,0 +1,15 @@
+// Spread operators
+//arrays:
+let names = ['Anna', 'Beata', 'Cecilia'];
+
+let moreNames = ['David', 'Erik', 'Fredrik'];
+
+// The classic (before spread operators) way to concatenate arrays
+let allNames = names.concat(moreNames);
+
+
+
+
+
+
+//objects:
